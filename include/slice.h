de9d5aa1acd6c84651a0e0345e690d9d465c53bb
Ryan Tsai <ryantsai90373@gmail.com>
2026-06-12 11:29:45 +0800

    Add funcions encode and decode 32-bit int

--- include/slice.h
+++ include/slice.h
@@ -66,6 +66,8 @@ public:
     // 2. The string itself contains \0, thus we might stop before going through the whole string
     std::string ToString() const { return std::string(data_, size_); }
 
+    bool remove_prefix(size_t n) { data_ += n; size_ -= n; }
+
 private:
     const char* data_;
     size_t size_;
---
+++ util/coding.cc
@@ -0,0 +1,44 @@
+#include <cstdint>
+#include "../include/slice.h"
+
+
+namespace leveldb_clone
+{
+    
+char* EncodeVarint32(char* dst, uint32_t v) {
+    // 0x80 = 1000 0000
+    while (v >= 0x80) {
+        // It is impossible to just copy last 7 bits
+        // we need to utilize bit operation not memcpy
+        *dst = (v & 0x7F) | 0x80;
+        dst++;
+        v >>= 7;
+    }
+    *dst = v;
+    dst++;
+    return dst;
+}
+
+bool GetVarint32(Slice* input, uint32_t* value) {
+    // avoid OR with some garbage
+    *value = 0; 
+
+    // Since we add a flag bit to every byte while encoding,
+    // a 32-bit int can be encoded into up to 5 bytes.
+    for (int i = 0; i < 5; i++) {
+        // ran out of input before the varint ended. 
+        if (input->size() == 0)
+            return false;
+        uint8_t byte = static_cast<uint8_t>(*input->data());
+
+        *value |= (byte & 0x7F) << (7 * i);
+        input->remove_prefix(1);
+        // If MSB is 0, then it is finished
+        if ((byte & 0x80) == 0)
+            return true; 
+    }
+    return false;
+}                                          
+
+} // namespace leveldb_clone
+
